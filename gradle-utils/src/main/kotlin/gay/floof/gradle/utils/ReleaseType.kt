ce1e28644ae7540b53e99b5ad3d3e1d078fb2d7f
Noel <cutie@floofy.dev>
2022-04-29 20:40:00 -0700

    fix: heading file

--- gradle-utils/src/main/kotlin/gay/floof/gradle/utils/ReleaseType.kt
+++ gradle-utils/src/main/kotlin/gay/floof/gradle/utils/ReleaseType.kt
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
  * 🤹 common-utils: Common Kotlin utilities made for my personal usage.
-=======
->>>>>>> 07746b610a6a181a7a89382249d23d3bdab2c0ef
- * Copyright (c) 2021-2022 Noel <cutie@floofy.dev>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
